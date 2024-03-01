4616aa442ad8394dfafa8e621a6e1f44a31e42e7
realfakenerd <lucas210898@hotmail.com>
2024-03-01 18:01:39 -0300

    docs(app): #12 base documentation route "/docs"

--- site/src/routes/docs/+page.svelte
+++ site/src/routes/docs/+page.svelte
@@ -2,18 +2,23 @@
 	import { Card } from '$lib/components/containment/card';
 
 	export let data;
-
-	console.log(data);
 </script>
 
-<section>
-	{#each data.docs as doc}
-		<Card outlined>
-			<figure><img src={doc.thumb} alt="thumbnail for {doc.title}" /></figure>
+<h1 class="text-display-large">Overview</h1>
 
-			<div class="bg-surface">
-				{doc.description}
-			</div>
-		</Card>
+<section class="grid">
+	{#each data.docs as doc}
+		<a class="contents group" href="/docs/{doc.slug}" style:--clr={doc.theme}>
+			<Card class="w-[394px] h-[336px] p-0 bg-surface-variant/10 group-hover:bg-[var(--clr)] transition-colors duration-500">
+				<figure class="bg-[var(--clr)] rounded-b-3xl group-hover:bg-surface transition-colors duration-500">
+					<img style="height: 197px;" src={doc.thumb} alt="thumbnail for {doc.title}" />
+				</figure>
+
+				<div class=" h-full p-8 ">
+					<h2 class="text-headline-medium">{doc.title}</h2>
+					<p class="text-body-large">{doc.description}</p>
+				</div>
+			</Card>
+		</a>
 	{/each}
 </section>
