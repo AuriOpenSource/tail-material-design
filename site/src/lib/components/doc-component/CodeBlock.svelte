435c0df082fea0b321668309dd82df171fbeb281
Roan Lopes <roan.lopes10@hotmail.com>
2024-03-01 22:19:49 -0300

    rename component

---
+++ site/src/lib/components/doc-component/CodeBlock.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import hljs from 'highlight.js';
+	import { onMount } from 'svelte';
+
+	type $$Props = {
+		lang: 'html' | 'javascript';
+	};
+
+	export let lang: string;
+
+	onMount(() => {
+		hljs.highlightAll();
+	});
+</script>
+<div>
+	<pre><code class={lang}><slot/></code></pre>
+</div>
+
+<style>
+	.html, .javascript {
+		@apply p-0;
+	}
+</style>
