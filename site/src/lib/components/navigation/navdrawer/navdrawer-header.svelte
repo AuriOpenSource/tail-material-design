82034fec8f18367742dfd8fdad1c2bcf3fc7c9b9
realfakenerd <lucas210898@hotmail.com>
2024-03-06 10:42:11 -0300

    docs(app): #12 added navdrawer to the documentation site

---
+++ site/src/lib/components/navigation/navdrawer/navdrawer-header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type $$Props = HTMLAttributes<HTMLDivElement>;
+
+	let className: $$Props['class'] = undefined;
+	export { className as class };
+</script>
+
+<header class={cn('min-h-14 px-7 flex items-center text-title-small', className)} {...$$restProps}>
+	<slot />
+</header>
---
+++ site/src/lib/components/navigation/navdrawer/navdrawer-item.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import Icon, { type IconifyIcon } from '@iconify/svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type $$Props = HTMLAttributes<HTMLAnchorElement> & {
+		icon?: string | IconifyIcon;
+		badge?: string;
+		href: string;
+		currentRoute: string;
+	};
+
+	export let className: $$Props['class'] = '';
+	export let icon: string | IconifyIcon = '';
+	export let href: string = '';
+	export let badge: string | null = null;
+	export let currentRoute: string = '';
+
+	export { className as class };
+</script>
+
+<a {href} class={cn('flex items-center min-h-14 gap-3 pl-4 pr-6 rounded-full', 
+	currentRoute===href ? 'bg-secondary-container' : ''
+,className)}>
+	<Icon {icon} width="24px" />
+	<span class="flex-1">
+		<slot />
+	</span>
+	{#if badge}
+		<span>{badge}</span>
+	{/if}
+</a>
