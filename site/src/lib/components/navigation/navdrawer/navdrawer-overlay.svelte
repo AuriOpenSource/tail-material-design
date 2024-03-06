28d8dbe6653614ca4d76adce1e58cd8cf37501c0
realfakenerd <lucas210898@hotmail.com>
2024-03-06 10:38:21 -0300

    docs(app): #12 added navdrawer to the documentation site

---
+++ site/src/lib/components/navigation/navdrawer/navdrawer-overlay.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Dialog as SheetPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+	import { fade } from 'svelte/transition';
+
+	type $$Props = SheetPrimitive.OverlayProps;
+
+	let className: $$Props['class'] = undefined;
+	export let transition: $$Props['transition'] = fade;
+	export let transitionConfig: $$Props['transitionConfig'] = {
+		duration: 250
+	};
+	export { className as class };
+</script>
+
+<SheetPrimitive.Overlay
+	{transition}
+	{transitionConfig}
+	class={cn('fixed inset-0 z-10 bg-background/90 backdrop-blur-sm ', className)}
+	{...$$restProps}
+/>
