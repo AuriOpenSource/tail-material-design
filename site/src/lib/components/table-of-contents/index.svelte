cf6bfe2550aa0124036f5b2e995e6e83f185390b
realfakenerd <lucas210898@hotmail.com>
2024-03-11 12:34:31 -0300

    docs(app): #12 added table-of-contents to the

---
+++ site/src/lib/components/table-of-contents/index.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Tree from './tree.svelte';
+	import { createTableOfContents } from '@melt-ui/svelte';
+  
+	const {
+	  elements: { item },
+	  states: { activeHeadingIdxs, headingsTree },
+	} = createTableOfContents({
+	  selector: '#mdsvex',
+	  activeType: 'highest',
+	  scrollOffset: 60
+	});
+
+</script>
+  
+<div
+	class="flex flex-col w-full"
+>
+	<div class="overflow-y-auto">
+	  <p class="text-on-variant text-label-medium">On This Page</p>
+	  <nav>
+		{#key $headingsTree}
+		  <Tree
+			tree={$headingsTree}
+			activeHeadingIdxs={$activeHeadingIdxs}
+			{item}
+		  />
+		{/key}
+	  </nav>
+	</div>
+</div>
